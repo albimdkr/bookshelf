9acc224e99e6b24b09f2a8251db44c66c137c890
albimdkr <albimudakar04@gmail.com>
2023-06-16 11:10:20 +0700

    add nanoid

---
+++ src/handler.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { nanoid } = require('nanoid');
+const books = require('./books');
---
+++ src/routes.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+// , getAllBooksHandler, getBookById, editBookById, deleteBookByIdHandler
+// eslint-disable-next-line no-unused-vars
+const { addBookHandler } = require('./handler');
