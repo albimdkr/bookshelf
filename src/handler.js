3f1a1d5cf0254f2051e3bb08a50ef6bfba73569b
albimdkr <albimudakar04@gmail.com>
2023-06-19 19:10:10 +0700

    add handler

--- src/handler.js
+++ src/handler.js
@@ -2,3 +2,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { nanoid } = require('nanoid');
 const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+};
+
+module.exports = { addBookHandler };
