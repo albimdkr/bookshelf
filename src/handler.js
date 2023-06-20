3dde1b3303bfaca22b2424bacaea88aedbbd4531
albimdkr <albimudakar04@gmail.com>
2023-06-20 20:00:10 +0700

    add insertAt

--- src/handler.js
+++ src/handler.js
@@ -5,6 +5,10 @@ const books = require('./books');
 
 const addBookHandler = (request, h) => {
   const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
 };
 
 module.exports = { addBookHandler };
